c01d76bfbc3b807d117fa660054adec129741de5
Samuelson Owei <97484454+Sammyowei@users.noreply.github.com>
2023-11-21 04:40:47 +0100

    Started all_english_word package

---
+++ example/all_english_words_example.dart
@@ -0,0 +1,6 @@
+import 'package:all_english_words/all_english_words.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/all_english_words_test.dart
@@ -0,0 +1,16 @@
+import 'package:all_english_words/all_english_words.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
